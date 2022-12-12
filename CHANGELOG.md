14812ad4ac73960ae0641501513cffcae6dd5c92
Mike <894659061@qq.com>
2022-12-12 15:13:08 +0800

    导出库

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
 ## 0.0.1
 
 * First commit
-
-## 0.0.1+2
-
-* Add tools
--- lib/stack_board_item.dart
+++ lib/stack_board_item.dart
@@ -1,1 +1,8 @@
 library widget_style_extension;
+
+export 'src/ex_ locale.dart';
+export 'src/ex_offset.dart';
+export 'src/ex_size.dart';
+export 'src/ex_text_height_behavior.dart';
+export 'src/ex_text_style.dart';
+export 'src/stack_text_strut_style.dart';
